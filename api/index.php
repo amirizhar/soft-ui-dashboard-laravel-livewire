22bee647f2a604dbbd3ec23ca8797372057b82f1
amirizhar <amir_izhar@yahoo.com>
2025-01-28 21:41:48 +0800

    update

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+// api/index.php
+
+// Point to the Laravel entry point
+require __DIR__ . '/../public/index.php';
--- resources/views/layouts/footers/guest/with-socials.blade.php
+++ resources/views/layouts/footers/guest/with-socials.blade.php
@@ -50,9 +50,8 @@
           <div class="row">
               <div class="col-8 mx-auto text-center mt-1">
                   <p class="mb-0 text-secondary">
-                      Copyright © {{ now()->year }} Soft by <a style="color: #252f40;" href="https://www.creative-tim.com"
-                          class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp; <a style="color: #252f40;"
-                          href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>
+                        Modified by <a style="color: #252f40;" href="https://www.creative-tim.com"
+                          class="font-weight-bold ml-1" target="_blank">Amir Izhar</a>
               </div>
               </p>
           </div>
